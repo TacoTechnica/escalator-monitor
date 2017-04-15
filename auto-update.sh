89044828ab9435d08f5619afad0a2c9adeeb1d0b
adrisj7 <adrisj7@gmail.com>
2017-04-15 19:04:49 +0000

    Update updater to also restart the systemctl escalator-updater after it pulls the repo (it will update in functionality too!

--- auto-update.sh
+++ auto-update.sh
@@ -2,17 +2,9 @@
 while true; do
     diff=$(git diff deploy)
     len=${#diff}
-    echo "diff: $diff"
-    echo "len: $len"
     if [ "$len" -ne "0" ] ; then
         echo "CHANGE DETECTED"
         git pull --no-edit origin deploy
+        sudo systemctl try-restart escalator
     fi
-    # There's gotta be a better way to do this
-    #git fetch
-    #if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ] ; then
-    #    echo "Change detected! Pulling..."
-    #    git checkout deploy
-    #    git pull origin deploy
-    #fi
 done
