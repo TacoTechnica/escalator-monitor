8b4da8b2f2cc960103c29747127b9f4e97788bb1
adrisj7 <adrisj7@gmail.com>
2017-04-15 18:32:51 +0000

    Change auto-update to its most primitive and janky form, now it should work...

--- auto-update.sh
+++ auto-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    git pull # There's gotta be a better way to do this
+    git pull origin deploy# There's gotta be a better way to do this
     #git fetch
     #if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ] ; then
     #    echo "Change detected! Pulling..."
