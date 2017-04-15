a5cf283f21b4b52b5518312d8f51b86c90852b3c
adrisj7 <adrisj7@gmail.com>
2017-04-15 15:09:06 -0400

    Add small debug for auto-update

--- auto-update.sh
+++ auto-update.sh
@@ -2,6 +2,7 @@
 while true; do
     diff=$(git diff deploy)
     len=${#diff}
+    echo "length: $len"
     if [ "$len" -ne "0" ] ; then
         echo "CHANGE DETECTED"
         git pull --no-edit origin deploy
