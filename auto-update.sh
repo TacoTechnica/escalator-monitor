ecaa1101559a036b8764086696e541cfe87a5a3f
adrisj7 <adrisj7@gmail.com>
2017-04-15 17:57:39 +0000

    Update auto-update to work with deploy branch

--- auto-update.sh
+++ auto-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do
     git fetch
-    if $(git rev-parse HEAD) == $(git rev-parse @{u}); then
-        git pull
+    if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ] ; then
+        git pull origin deploy
     fi
 done
