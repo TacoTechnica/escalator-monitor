60f0fdef19d5c352ecab2a8f78fb563e1420edec
adrisj7 <adrisj7@gmail.com>
2017-04-15 18:26:44 +0000

    Add small prompt to auto-update

--- auto-update.sh
+++ auto-update.sh
@@ -2,6 +2,8 @@
 while true; do
     git fetch
     if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ] ; then
+        echo "Change detected! Pulling..."
+        git checkout deploy
         git pull origin deploy
     fi
 done
