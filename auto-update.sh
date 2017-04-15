d8dce72ecc2c402fa04f8cc511d302e72d663814
adrisj7 <adrisj7@gmail.com>
2017-04-15 19:33:12 +0000

    Update auto-update to properly address the git repo for every git command

--- auto-update.sh
+++ auto-update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 while true; do
-    git fetch origin deploy
-    diff=$(git diff origin/deploy)
+    env -i git fetch origin deploy
+    diff=$(env -i git diff origin/deploy)
     len=${#diff}
     echo "length: $len"
     if [ "$len" -ne "0" ] ; then
         echo "CHANGE DETECTED"
-        git pull --no-edit origin deploy
+        env -i git pull --no-edit origin deploy
         sudo systemctl try-restart escalator
     fi
 done
