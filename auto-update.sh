fa7d9c08059ff36c7fcbe6dcedcbe01dfd21c941
adrisj7 <adrisj7@gmail.com>
2017-04-15 18:35:25 +0000

    Add --no-edit flag to primitive auto-pull

--- auto-update.sh
+++ auto-update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 while true; do
-    git pull origin deploy# There's gotta be a better way to do this
+    git pull --no-edit origin deploy
+    # There's gotta be a better way to do this
     #git fetch
     #if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ] ; then
     #    echo "Change detected! Pulling..."
